f20f16de2c8b9f33791e31b67e94a61b79035598
Rui Vieira <revs87@msn.com>
2024-03-18 00:35:05 +0000

    Size tweak

--- site/src/jsMain/kotlin/pt/rvcoding/personalwebsitecomposehtml/util/Res.kt
+++ site/src/jsMain/kotlin/pt/rvcoding/personalwebsitecomposehtml/util/Res.kt
@@ -46,7 +46,7 @@ object Res {
     object Dimens {
         const val BORDER_RADIUS = 8
         const val MAX_CARD_WIDTH = 1200
-        const val MAX_CARD_HEIGHT = 900
+        const val MAX_CARD_HEIGHT = 920
         const val ICON_SIZE = 24
         const val ICON_SIZE_LG = 32
     }
